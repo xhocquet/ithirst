c80eb383fa54afab51fc70fea1075ffdcf19b9b7
xhocquet <xhocquet@gmail.com>
2015-03-11 00:46:00 -0400

    layout fixes, chart js replaced google

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -1,22 +1,45 @@
-google.load("visualization", "1", {packages:["corechart"]});
-google.setOnLoadCallback(drawChart);
+var ctx = document.getElementById("ingredientChart").getContext("2d");
+var data = [
+    {
+        value: 3,
+        color:"#F5F39D",
+        label: "Tequila"
+    },
+    {
+        value: 2,
+        color: "#F7F7E1",
+        label: "Triple Sec"
+    },
+    {
+        value: 1,
+        color: "#66DE2F",
+        label: "Lime Juice"
+    }
+]
+var options ={
+    //Boolean - Whether we should show a stroke on each segment
+    segmentShowStroke : false,
 
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work',     11],
-    ['Eat',      2],
-    ['Commute',  2],
-    ['Watch TV', 2],
-    ['Sleep',    7]
-  ]);
+    //String - The colour of each segment stroke
+    segmentStrokeColor : "#fff",
 
-  var options = {
-    'pieHole': 0.4,
-    'legend': 'none',
-    'backgroundColor': { fill:'transparent' }
-  };
+    //Number - The width of each segment stroke
+    segmentStrokeWidth : 2,
 
-  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-  chart.draw(data, options);
+    //Number - The percentage of the chart that we cut out of the middle
+    percentageInnerCutout : 50, // This is 0 for Pie charts
+
+    //Number - Amount of animation steps
+    animationSteps : 80,
+
+    //String - Animation easing effect
+    animationEasing : "easeOutCubic",
+
+    //Boolean - Whether we animate the rotation of the Doughnut
+    animateRotate : true,
+
+    //Boolean - Whether we animate scaling the Doughnut from the centre
+    animateScale : false
 }
+
+var ingredients = new Chart(ctx).Doughnut(data,options);
